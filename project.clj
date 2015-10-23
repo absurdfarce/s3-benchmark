4702acf9a825c1d35e11ae6033151d4e3bcfece1
Bret McGuire <bret.mcguire@datastax.com>
2015-10-23 16:33:38 -0500

    Extracting reporting from the base flow, trying to move base functionality to something which composes easily.  Also added a trivial jclouds target, just to verify base functionality.  Finally, shifting things over to use our jclouds fork.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject s3_benchmark "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.generators "0.1.2"]
-                 [org.apache.jclouds/jclouds-all "1.9.1"]
+                 [org.apache.jclouds/jclouds-blobstore "1.8.0" :exclusions [org.apache.jclouds/jclouds-core]]
+                 [arre.jclouds/jclouds-core "1.8.0"]
+		 [arre.jclouds.provider/aws-s3 "1.8.0" :exclusions [org.apache.jclouds/jclouds-core]]
                  [org.clojure/tools.logging "0.3.1"]
                  [amazonica "0.3.33"]
                  [clj-time "0.11.0"]]
--- src/s3_benchmark/core.clj
+++ src/s3_benchmark/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.tools.logging :as log]
             [clj-time.core :as time]
             [clj-time.format :as format]
+            [s3-benchmark.analyze :as analyze]
             [s3-benchmark.conf :as conf]
             [s3-benchmark.amazonica :as amazonica]
             [s3-benchmark.jclouds :as jclouds]
@@ -17,27 +18,22 @@
         {:name 'upload' :wall-time 0 :free-mem-delta 0 :total-mem-delta 0}
         {...}]
     }"
+  (println "Running test, chunk size: " chunk-size ", chunk count: " chunk-count)
   (let [local-tmp-file (util/generate-file chunk-size chunk-count)
-        tmpdir (java.io.File. (System/getProperty "java.io.tmpdir") (util/random-uuid-string)) ;; unique place to download
+        tmpdir (java.io.File. (System/getProperty "java.io.tmpdir") (util/random-uuid-string))
         downloaded-file (java.io.File. tmpdir (.getName local-tmp-file))
-        test-data (transient {:timestamp (format/unparse (format/formatters :rfc822) (time/now))
+        test-data {:timestamp (format/unparse (format/formatters :rfc822) (time/now))
                               :params (dissoc test-params :creds :upload-fn :download-fn)
-                              :test-file {:name (.getAbsolutePath local-tmp-file)
-                                          :size (.length local-tmp-file)}})]
-    (try
-      (do
-        (assoc! test-data
-                :transfers
+                              :test-file {:name (.getName local-tmp-file)
+                                          :size (.length local-tmp-file)}}
+        transfers
                 (-> []
                     (util/record "upload" #(upload-fn creds bucket local-tmp-file))
-                    (util/record "download" #(download-fn creds bucket local-tmp-file tmpdir))))
+                    (util/record "download" #(download-fn creds bucket local-tmp-file tmpdir)))]
         (util/verify-files-match local-tmp-file downloaded-file)
         (.delete downloaded-file)
         (.delete local-tmp-file)
-        (assoc! test-data :result "success" :exception nil))
-      (catch Throwable ex (assoc! test-data :result "error" :exception (.getMessage ex))))
-    (persistent! test-data)))
-
+        (assoc test-data :transfers transfers)))
 
 (defn- multiple-file-test
   [test-runs test-params]
@@ -46,7 +42,6 @@
       (log/infof "starting iteration %s" i)
       (Thread/sleep (util/random-long 10000 90000))
       (single-file-test test-params))))
-
 
 (defn build-test-executor
   "Usage:   (run-build-test-executor test-fn & test-fn-args)
@@ -57,11 +52,7 @@
   for the definition of a number of repeatable test cases that save the results to disk for further
   analysis."
   [test-fn & test-fn-args]
-  (fn [result-file]
-    (let [result (apply test-fn test-fn-args)]
-      (spit (java.io.File. conf/default-report-dir result-file)
-            (with-out-str (pr result))))))
-
+  (fn [] (apply test-fn test-fn-args)))
 
 ;; Chunk Size / Chunk Count Table
 ;;
@@ -159,6 +150,12 @@
                                                                                             :chunk-count 122071
                                                                                             :upload-fn   jclouds/upload-file
                                                                                             :download-fn jclouds/download-file})
+                             :single-really-small-file (build-test-executor single-file-test {:creds       conf/default-creds
+                                                                                       :bucket      conf/default-bucket
+                                                                                       :chunk-size  256
+                                                                                       :chunk-count 40
+                                                                                       :upload-fn   jclouds/upload-file
+                                                                                       :download-fn jclouds/download-file})
                              :single-small-file (build-test-executor single-file-test {:creds       conf/default-creds
                                                                                        :bucket      conf/default-bucket
                                                                                        :chunk-size  (util/random-long 256 512)
@@ -183,10 +180,14 @@
   global map. The resulting data structure is then written out with a generated filename to the configured
   report directory."
   [lib-type test-case]
-  (let [filename-format (format/formatter "YYYYMMdd_HHmm")
-        date-time (format/unparse filename-format (time/now))
-        output-file (str date-time "_" (util/keyword->filename lib-type) "_" (util/keyword->filename test-case) ".edn")
-        test-fn (-> test-cases
+  (let [test-fn (-> test-cases
                     (get lib-type)
-                    (get test-case))]
-    (test-fn output-file)))
+                    (get test-case))
+        results (test-fn)
+        download-data (analyze/transform-download-data results)
+        download-speed (analyze/compute-download-speed download-data)]
+     (println "Raw results: " results)
+     (println "Download data: " download-data)
+     (println "Download speed: " download-speed)
+    download-speed))
+
